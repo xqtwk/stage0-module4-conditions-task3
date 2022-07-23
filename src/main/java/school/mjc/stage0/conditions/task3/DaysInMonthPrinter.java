84d6b3ee9ebe31573b1b59e4fe5a714a5cf0196d
Richard <xqtwkk@gmail.com>
2022-07-23 18:21:41 +0300

    Attempt 2

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -4,13 +4,13 @@ public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
         int year = 2022;
         if ((month==2) && ((year%4==0) || ((year%100==0)&&(year%400==0)))) {
-            System.out.println("Number of days is 29");
+            System.out.println("29");
         } else if (month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) {
-            System.out.println("Number of days is 31");
+            System.out.println("31");
         } else if(month==2) {
-            System.out.println("Number of days is 28");
+            System.out.println("28");
         } else {
-            System.out.println("Number of days is 30");
+            System.out.println("30");
         }
     }
 }
